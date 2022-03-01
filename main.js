ca6dda3f55532d0f7e162ed7c5a80cd2110ac532
Ozzymand <24418935+Ozzymand@users.noreply.github.com>
2022-03-01 18:30:10 +0200

    Added automatic cell color for dates
    Still testing on main :)

--- main.js
+++ main.js
@@ -4,6 +4,33 @@ let year = date.getYear();
 let month = date.getMonth();
 let cont = document.getElementById('container');
 
+let url = 'https://raw.githubusercontent.com/Ozzymand/UPBCalendar/main/date.json';
+
+fetch(url)
+    .then(response => response.json())
+    .then(inputData => {
+      for(var i = 0; i < inputData.examens.length; i++){
+        var data = inputData.examens[i].data.split('-');
+        var materie = inputData.examens[i].materie;
+        var ora = inputData.examens[i].ora;
+        var sala = inputData.examens[i].sala;
+        var color = inputData.examens[i].color;
+
+        // search for the date in calendar
+        var dateIn = document.getElementById(`${data[0]-1}-${data[1]-1}`);
+        dateIn.style = `background-color: ${color}`;
+
+      }
+      for(var i = 0; i < inputData.teste.length; i++){
+        var data = inputData.teste[i].data.split('-');
+        var materie = inputData.teste[i].materie;
+        var color = inputData.teste[i].color;
+        
+        // search for the date in calendar
+        var dateIn = document.getElementById(`${data[0]-1}-${data[1]-1}`);
+        dateIn.style = `background-color: ${color}`;
+      }
+    });
 
 function makeMonth(monthNum) {
   var newMonth = document.createElement('div');
@@ -22,6 +49,7 @@ function makeMonth(monthNum) {
   let daysInMonth = new Date(year, monthNum+1, 0).getDate();
   for(var i = 0; i < daysInMonth; i++){
     var li = document.createElement('li');
+    li.id = `${i}-${monthNum}`;
     li.appendChild(document.createTextNode(i+1));
     ul.appendChild(li);
   }
@@ -31,5 +59,3 @@ function makeMonth(monthNum) {
 for(var i = 0; i < 12; i++){
   makeMonth(i);
 }
-
-console.log(JSON.stringify(dateUni));
